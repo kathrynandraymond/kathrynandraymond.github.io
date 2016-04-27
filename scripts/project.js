27ff7e5a2ef38e32d6ac403483f51f20fbba6378
Raymond Chan <raymondschan@gmail.com>
2016-04-26 22:30:28 -0700

    Hide overflowing text

--- scripts/project.js
+++ scripts/project.js
@@ -468,10 +468,39 @@ Wedding.modules = Wedding.modules || {};
 Wedding.modules.TravelInfo = function() {
 	var that = this;
 
-	this.init = function(parentElement) {
-	
+    var parentElement = null;
+
+    this.init = function(parentElement) {
+    	that.parentElement = parentElement;
+    	hideTooMuch();
 	};
+
+	var hideTooMuch = function() {
+		var topics = $(that.parentElement).find('.topic');
+		var anyTooLong = false;
+		for(var i = 0, len = topics.length; i < len; i++) {
+			var infoBlock = $(topics[i]).find('.info');
+			var innerBlock = $(infoBlock).find('.inner');
+			if($(infoBlock).height() < $(innerBlock).height()) {
+				anyTooLong = true;
+				$(topics[i]).addClass('overflown');
+			}
+		}
+
+		if(anyTooLong) {
+			$('.seeMore,.hideOverflow').click(function(e) {
+				var topic = $(e.target).parents('.topic');
+				if($(e.target).hasClass('seeMore')) {
+					$(topic).addClass('exempted').removeClass('overflown');
+				} else if($(e.target).hasClass('hideOverflow')) {
+					$(topic).removeClass('exempted').addClass('overflown');
+				}
+				return false;
+			});
+		}
+    }
 };
+
 
 var initializedObjects = {};
 
